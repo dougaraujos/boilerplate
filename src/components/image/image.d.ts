20a0df0287d9df1caa79478f91f866d319c1bd35
Douglas <doug.araujos@gmail.com>
2021-03-10 20:10:55 -0300

    Add image component definitions

---
+++ src/components/image/image.d.ts
@@ -0,0 +1,18 @@
+/**
+ * IMPORTS
+ */
+import { ImageProps } from 'next/image';
+
+
+/**
+ * Image properties type.
+ */
+export type IProps = ImageProps
+
+
+/**
+ * Image default properties.
+ */
+export const defaultProps: IProps = {
+  layout: 'fill'
+};
