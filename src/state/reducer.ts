6dc8daea9a45568bbc7c1e76bf64567036bcd601
Douglas <doug.araujos@gmail.com>
2021-03-11 15:01:53 -0300

    Add root reducer

---
+++ src/state/reducer.ts
@@ -0,0 +1,16 @@
+/**
+ * IMPORTS
+ */
+import { combineReducers } from 'redux';
+
+
+/**
+ * I create a root reducer.
+ */
+const reducer = combineReducers({});
+
+
+/**
+ * EXPORTS
+ */
+export default reducer;
