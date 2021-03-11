62d3ebaefc3dd9126636006daa5f750ee408a651
Douglas <doug.araujos@gmail.com>
2021-03-11 15:30:24 -0300

    Add user initial state

---
+++ src/features/user/initialstate.ts
@@ -0,0 +1,21 @@
+/**
+ * IMPORTS
+ */
+import { IUserState } from './index.d';
+
+
+/**
+ * I am the user initial state.
+ */
+const initialState: IUserState = {
+  id: null,
+  email: '',
+  status: null,
+  username: ''
+};
+
+
+/**
+ * EXPORTS
+ */
+export { initialState };
