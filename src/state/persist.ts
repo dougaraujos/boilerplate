e561403d06b9f3b9317c6077abd2a180cb94eb07
Douglas <doug.araujos@gmail.com>
2021-04-08 12:53:17 -0300

    Add notify to persistence blacklist

--- src/state/persist.ts
+++ src/state/persist.ts
@@ -3,12 +3,14 @@
  */
 import { persistReducer, persistStore, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage/session';
+import { notify } from 'infra/notify';
 
 
 /**
  * I configure a persisted redux store.
  */
 const persistConfig: PersistConfig<unknown> = {
+  blacklist: [notify.name],
   key: 'onlydiamonds',
   storage
 };
