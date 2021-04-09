56b697bbbcc00f2a7808ad2df8de8e9e5a7023ab
Douglas <doug.araujos@gmail.com>
2021-04-09 20:23:49 -0300

    Add error handler to http request

---
+++ src/infra/http/handlers.ts
@@ -0,0 +1,35 @@
+/**
+ * IMPORTS
+ */
+import { actions } from 'infra/notify';
+
+
+/**
+ * I handle a request error.
+ */
+const handleError =
+  async (exception, data, dispatch): Promise<unknown> => {
+    // data available: notify and return
+    if (data !== undefined) {
+      return dispatch(actions.error(data));
+    }
+
+    try {
+      // get error details
+      const error = await exception.json();
+
+      // dispatch action to notify error
+      dispatch(actions.error(error));
+    } catch (e) {
+      // dispatch action to notify failed request
+      dispatch(actions.error({
+        title: process.env.NEXT_PUBLIC_ERROR_REQUEST_FAILED
+      }));
+    }
+  };
+
+
+/**
+ * EXPORTS
+ */
+export { handleError };
